11a96988d72b3d39e9c2cb182ff00d4a1515a1fa
Leandro Scalabrini <scalabrinileandro12@gmail.com>
2023-04-03 20:52:13 -0300

    first commit

---
+++ 2/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        double numero;
+        System.out.println("Digite um número qualquer:");
+        Scanner teclado = new Scanner(System.in);
+        numero = teclado.nextDouble();
+        System.out.println("O número digitado é: " +numero);
+    }
+}
